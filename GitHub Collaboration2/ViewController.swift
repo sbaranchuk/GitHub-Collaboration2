4db495c7e233d930a8855f8c6213c5a61f506e3e
sbaranchuk <sbaranchuk@protonmail.com>
2022-11-30 18:43:21 +0300

    Initial Commit

---
+++ GitHub Collaboration2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub Collaboration2
+//
+//  Created by Admin on 30/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
